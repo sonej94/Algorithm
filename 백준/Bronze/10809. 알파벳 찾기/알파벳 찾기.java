84ef43ebc7343657edd33c6d6080db5f4e6eb163
sonej94 <sonej94@gmail.com>
2024-09-26 21:29:55 +0900

    [Bronze II] Title: 알파벳 찾기, Time: 112 ms, Memory: 15860 KB -BaekjoonHub

---
+++ 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		
+		String s = br.readLine();
+		
+		for(char c = 'a'; c<='z'; c++ ) {
+			sb.append(s.indexOf(c)+" ");
+		}
+	
+		System.out.println(sb);
+
+			
+		}
+
+}
