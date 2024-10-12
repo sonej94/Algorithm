395e84e6298824ce591527da7594af9e7c033e4d
sonej94 <sonej94@gmail.com>
2024-10-13 04:15:57 +0900

    [Silver II] Title: 그르다 김가놈, Time: 536 ms, Memory: 93920 KB -BaekjoonHub

---
+++ 백준/Silver/18113. 그르다 김가놈/그르다 김가놈.java
@@ -0,0 +1,54 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class Main {
+  public static void main(String[] args) throws IOException {
+      
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    StringTokenizer st = new StringTokenizer(br.readLine());
+      
+    int n = Integer.parseInt(st.nextToken());
+    int k = Integer.parseInt(st.nextToken());
+    int m = Integer.parseInt(st.nextToken());
+      
+    List<Integer> list = new ArrayList<>();
+    int max = 0;
+
+    for (int i = 0; i < n; i++) {
+      int value = Integer.parseInt(br.readLine());
+        
+      if (value > (2 * k)) {
+        list.add(value - (2 * k));
+        max = Math.max(max, value - (2*k));
+          
+      } else if (2*k > value && value > k) {
+        list.add(value - k);
+        max = Math.max(max, value - k);
+      }
+    }
+
+    int start = 1;
+    int end = max;
+    int result = -1;
+
+    while (end - start >= 0) {
+      int mid = (end + start) / 2;
+
+      int sum = 0;
+      for (int a : list) {
+        sum += a / mid;
+      }
+      if (sum >= m) {
+        result = mid;
+        start = mid + 1;
+      } else {
+        end = mid - 1;
+      }
+    }
+    System.out.println(result);
+  }
+}
