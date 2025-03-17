3beb08935422c2c71deb44f88568163ce925109b
sonej94 <sonej94@gmail.com>
2025-03-18 00:11:31 +0900

    [Bronze V] Title: 초코바, Time: 112 ms, Memory: 14200 KB -BaekjoonHub

---
+++ 백준/Bronze/27959. 초코바/초코바.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int N = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
+		 
+		String result;
+
+		if( 100*N >= M) {
+			result = "Yes";
+		}else {
+			result = "No";
+		}
+		
+		System.out.println(result);
+	}
+
+}
