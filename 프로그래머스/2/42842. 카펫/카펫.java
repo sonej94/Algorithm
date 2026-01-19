0bf213c2bc5becafcefc1bc0d2117f5e1dff50e4
sonej94 <sonej94113@naver.com>
2026-01-19 22:23:18 +0900

    [level 2] Title: 카펫, Time: 0.05 ms, Memory: 72 MB -BaekjoonHub

---
+++ 프로그래머스/2/42842. 카펫/카펫.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int brown, int yellow) {
+        
+        int total = brown + yellow;
+        
+        for(int h = 1; h *h <= total; h++){
+            
+            if (total % h != 0) continue;
+            
+            int w = total / h;
+            if (w < h) continue;
+            
+            if ((w - 2) * (h - 2) == yellow){
+                 return new int[]{w, h};
+            }
+            
+        }
+
+        return new int[]{};
+    }
+}
