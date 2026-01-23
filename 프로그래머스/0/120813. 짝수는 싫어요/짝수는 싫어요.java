6259941fa79831ec707b6bf3793f8156d4c48acf
sonej94 <sonej94113@naver.com>
2026-01-23 23:58:39 +0900

    [level 0] Title: 짝수는 싫어요, Time: 0.03 ms, Memory: 75.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/120813. 짝수는 싫어요/짝수는 싫어요.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int n) {
+        int[] answer = new int[(n+1)/2];
+        int idx = 0;
+        
+        for(int i=1; i<=n; i++){
+            if(i%2 != 0){
+                answer[idx++] = i;
+            }
+        }
+        
+        return answer;
+    }
+}
